adb0eadc8d244d6a36946daaa93689dde4536500
cwllwch <162603682+cwllwch@users.noreply.github.com>
2025-11-24 22:10:52 +0200

    fixed couple bugs

--- lib/JsonParser/lumberjack/node_processor.ex
+++ lib/JsonParser/lumberjack/node_processor.ex
@@ -246,6 +246,11 @@ defmodule JsonParser.Lumberjack.NodeProcessor do
   defp get_value([first | new_list] = _list, key) when is_comma_or_bracket(first) do
     evaluate_value_type(new_list, key)
   end
+  
+  defp get_value([first, second, third | _new_list] = list, key) when 
+    is_string(first, second, third) do
+    evaluate_value_type(list, key)
+  end
 
   # Idea here is to move forward in case of no pattern recognized.
   # This approach is easier than ignoring all the escapes and
@@ -668,7 +673,7 @@ defmodule JsonParser.Lumberjack.NodeProcessor do
   end
 
   defp get_separator([first | tail] = _list, key_val, acc) do
-    Logger.debug("ignoring value #{inspect(first)}")
+    Logger.debug("ignoring before separator #{inspect(first)}")
     get_separator(tail, key_val, acc)
   end
 
